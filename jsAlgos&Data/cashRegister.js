35f660396c0932bfda982e3946f0e07539f831b7
FranklinMiranda <fdm61344@gmail.com>
2022-05-05 23:59:33 -0400

    com

--- jsAlgos&Data/cashRegister.js
+++ jsAlgos&Data/cashRegister.js
@@ -3,48 +3,52 @@ console.log(new Date(2022, 4, 5, 19, 34));
 
 function checkCashRegister(price, cash, cid) {
   cid = cid.reverse();
-  let balance = cash - price;
-  let denominationVal = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-  let changeQuantity = cid.map(function (val) {
+  let balanceVal = cash - price;
+  let denomArr = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  let changeQuantArr = cid.map(function (val) {
     return parseFloat(val[1]);
   });
-  let changeTotal = cid.reduce(function (total, val) {
+  let changeTotVal = cid.reduce(function (total, val) {
     return (total += val[1]);
   }, 0);
-  let result = [];
-
-  if (changeTotal < balance) {
-    return { status: 'INSUFFICIENT_FUNDS', change: [] };
-  } else if (changeTotal === balance) {
-    return { status: 'CLOSED', change: cid };
+  let resultObj = { status: 'DEFAULT', change: [] };
+  if (changeTotVal < balanceVal) {
+    resultObj.status = 'INSUFFICIENT_FUNDS';
+    return resultObj;
+  } else if (changeTotVal == balanceVal) {
+    resultObj.status = 'CLOSED';
+    resultObj.change = cid;
+    resultObj.change = resultObj.change.reverse();
+    return resultObj;
   }
 
-  denominationVal.forEach(function (val, i) {
-    let remainingDenomBalance = 0;
-    for (i; i < changeQuantity.length; i++) {
-      remainingDenomBalance += changeQuantity[i];
+  for (let i = 0; i < cid.length; i++) {
+    let remDenomVal = 0;
+    for (let j = i; j < changeQuantArr.length; j++) {
+      remDenomVal += changeQuantArr[j];
     }
+    remDenomVal = remDenomVal.toFixed(2);
 
-    console.log(remainingDenomBalance);
-
-    if (balance > remainingDenomBalance) {
-      return { status: 'INSUFFICIENT_FUNDS', change: [] };
-    } else if (balance >= val && cid[i][1] !== 0) {
-      const quant = Math.min(parseInt(balance / val + 0.01), cid[i][1] / val);
-
-      result.push([cid[i][0], quant * val]);
-      balance -= quant * val;
-      cid[i][1] -= quant * val;
+    if (balanceVal > remDenomVal) {
+      resultObj.status = 'INSUFFICIENT_FUNDS';
+      return resultObj;
+    } else if (balanceVal >= denomArr[i] && cid[i][1] !== 0) {
+      const quant = Math.min(parseInt((balanceVal + 0.01) / denomArr[i]), cid[i][1] / denomArr[i]);
       console.log(quant);
-      console.log(result);
-    }
-  });
 
-  return { status: 'OPEN', change: result };
+      resultObj.change.push([cid[i][0], quant * denomArr[i]]);
+      balanceVal -= quant * denomArr[i];
+
+      cid[i][1] -= quant * denomArr[i];
+      resultObj.status = 'OPEN';
+    }
+  }
+
+  return resultObj;
 }
 
 console.log(
-  checkCashRegister(19.5, 20, [
+  checkCashRegister(3.26, 100, [
     ['PENNY', 1.01],
     ['NICKEL', 2.05],
     ['DIME', 3.1],
@@ -56,3 +60,63 @@ console.log(
     ['ONE HUNDRED', 100],
   ])
 );
+
+// function checkCashRegister(price, cash, cid) {
+//   cid = cid.reverse();
+//   let returnedBalance = cash - price;
+//   let denominationVal = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+//   let changeQuantity = cid.map(function (val) {
+//     return parseFloat(val[1]);
+//   });
+//   let changeTotal = cid.reduce(function (total, val) {
+//     return (total += val[1]);
+//   }, 0);
+//   let resultObj = { status: '', change: [] };
+
+//   if (changeTotal < returnedBalance) {
+//     resultObj.status = 'INSUFFICIENT_FUNDS';
+//     return resultObj;
+//   } else if (changeTotal === returnedBalance) {
+//     resultObj.status = 'CLOSED';
+//     resultObj.change = cid;
+//     return resultObj;
+//   }
+
+//   denominationVal.forEach(function (val, index) {
+//     let remainingDenomBalance = 0;
+//     for (index; index < changeQuantity.length; index++) {
+//       remainingDenomBalance += changeQuantity[index];
+//     }
+//     remainingDenomBalance = remainingDenomBalance.toFixed(2);
+//     console.log(remainingDenomBalance);
+
+//     if (returnedBalance > remainingDenomBalance) {
+//       resultObj.status = 'INSUFFICIENT_FUNDS';
+//       return resultObj;
+//     } else if (returnedBalance >= val && cid[index][1] !== 0) {
+//       const quant = Math.min(parseInt(returnedBalance / val + 0.01), cid[index][1] / val);
+
+//       resultObj.push([cid[index][0], quant * val]);
+//       returnedBalance -= quant * val;
+//       cid[index][1] -= quant * val;
+//       console.log(quant);
+//       console.log(resultObj);
+//     }
+//   });
+
+//   return { status: 'OPEN', change: resultObj };
+// }
+
+// console.log(
+//   checkCashRegister(19.5, 20, [
+//     ['PENNY', 1.01],
+//     ['NICKEL', 2.05],
+//     ['DIME', 3.1],
+//     ['QUARTER', 4.25],
+//     ['ONE', 90],
+//     ['FIVE', 55],
+//     ['TEN', 20],
+//     ['TWENTY', 60],
+//     ['ONE HUNDRED', 100],
+//   ])
+// );
