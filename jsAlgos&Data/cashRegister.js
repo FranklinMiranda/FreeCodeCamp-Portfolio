05a96296220f4e011ef04b235ac50d5710059c61
FranklinMiranda <fdm61344@gmail.com>
2022-05-05 21:40:49 -0400

    com

--- jsAlgos&Data/cashRegister.js
+++ jsAlgos&Data/cashRegister.js
@@ -4,19 +4,14 @@ console.log(new Date(2022, 4, 5, 19, 34));
 function checkCashRegister(price, cash, cid) {
   cid = cid.reverse();
   let balance = cash - price;
-  console.log(balance);
-  console.log(cid);
-
-  let changeVal = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
-  let change = cid.map(function (val) {
+  let denominationVal = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01];
+  let changeQuantity = cid.map(function (val) {
     return parseFloat(val[1]);
   });
   let changeTotal = cid.reduce(function (total, val) {
     return (total += val[1]);
   }, 0);
-  console.log(changeVal);
-  console.log(change);
-  console.log(changeTotal);
+  let result = [];
 
   if (changeTotal < balance) {
     return { status: 'INSUFFICIENT_FUNDS', change: [] };
@@ -24,21 +19,15 @@ function checkCashRegister(price, cash, cid) {
     return { status: 'CLOSED', change: cid };
   }
 
-  let result = [];
-  changeVal.forEach(function (val, i) {
-    console.log(val);
-    console.log(i);
+  denominationVal.forEach(function (val, i) {
+    let remainingDenomBalance = 0;
+    for (i; i < changeQuantity.length; i++) {
+      remainingDenomBalance += changeQuantity[i];
+    }
 
-    let remainingBalance = change.reduce(function (total, value, index) {
-      if (index >= i) {
-        return (total += value);
-      }
-    }, 0);
-    console.log(remainingBalance);
+    console.log(remainingDenomBalance);
 
-    if (balance > 0 && val === 0) {
-      return { status: 'INSUFFICIENT_FUNDS', change: [] };
-    } else if (balance > remainingBalance) {
+    if (balance > remainingDenomBalance) {
       return { status: 'INSUFFICIENT_FUNDS', change: [] };
     } else if (balance >= val && cid[i][1] !== 0) {
       const quant = Math.min(parseInt(balance / val + 0.01), cid[i][1] / val);
@@ -56,18 +45,14 @@ function checkCashRegister(price, cash, cid) {
 
 console.log(
   checkCashRegister(19.5, 20, [
-    ['PENNY', 0.01],
-    ['NICKEL', 0],
-    ['DIME', 0],
-    ['QUARTER', 0],
-    ['ONE', 1],
-    ['FIVE', 0],
-    ['TEN', 0],
-    ['TWENTY', 0],
-    ['ONE HUNDRED', 0],
+    ['PENNY', 1.01],
+    ['NICKEL', 2.05],
+    ['DIME', 3.1],
+    ['QUARTER', 4.25],
+    ['ONE', 90],
+    ['FIVE', 55],
+    ['TEN', 20],
+    ['TWENTY', 60],
+    ['ONE HUNDRED', 100],
   ])
 );
-
-//   if (balance > cid[i][1]) {
-//     return { status: 'INSUFFICIENT_FUNDS', change: [] };
-//   }
